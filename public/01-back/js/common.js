e7dffd1814f3ef9194925b2efc88a6a8e0dad6ba
liangshen <319150264@qq.com>
2018-09-27 18:54:44 +0800

    后台各个页面公共木块布局完成

---
+++ public/01-back/js/common.js
@@ -0,0 +1,50 @@
+
+// 进度条功能
+
+$(function () {
+    $(document).ajaxStart(function () {
+      NProgress.start()
+    })
+
+    $(document).ajaxStop(function () {
+      NProgress.done();      
+    })
+})
+
+
+$(function () {
+  
+  // 二级菜单显示
+  $('.nav .category').click(function () {
+    $('.nav .child').stop().slideToggle()    
+  })
+
+  // 隐藏侧边栏
+
+  $('.lt_header .menu').click(function () {
+    $('.lt_side').toggleClass('current')
+    $('.lt_header').toggleClass('current')
+    $('.lt_main').toggleClass('current')
+  })
+
+
+  // 点击退出显示模态框功能
+
+  $('.lt_header .login-out').click(function () {
+    $('#modal').modal('show')
+  })
+
+  $('#lgout').click(function () {
+
+    $.ajax({
+      url:'/employee/employeeLogout',
+      dataType:'json',
+      success: function ( info ) {
+        console.log(info);
+        if(info.success) {
+          location.href = 'login.html'
+        }
+      }
+    })
+  })
+})
---
+++ public/01-back/js/islogin.js
@@ -0,0 +1,11 @@
+$(function () {
+  $.ajax({
+    url:"/employee/checkRootLogin",
+    dataType:'json',
+    success: function (info) {
+      if(info.error == 400){
+         location.href = "login.html"
+      }
+    }
+  })
+})
